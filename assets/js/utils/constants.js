11a47c9ee4bb4c710dfb6110ea0c248afd56ed74
Andrey Baranov <baranov.a.64103@gmail.com>
2024-06-04 07:08:49 +0200

    Deployment setup

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -1,3 +1,10 @@
 var Constants = {
-  API_BASE_URL: "http://localhost:8888/BlockWatch/rest",
+  get_api_base_url: function() {
+    if (window.location.hostname === "localhost") {
+      return "http://localhost:8888/BlockWatch/rest";
+    } else {
+      return "https://web-blockwatch-q785y.ondigitalocean.app/rest";
+    }
+  },
+  API_BASE_URL: this.get_api_base_url(),
 };
